b617044994aec961089e898094a4786ee86eefa4
Ammon Wanner <sonofthekingsman51@gmail.com>
2026-08-07 00:48:19 -0600

    I started making the side bar.

---
+++ ProductivityManager/Views/UserControls/SideBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProductivityManager.Views.UserControls;
+
+public partial class SideBar : UserControl
+{
+    public SideBar()
+    {
+        InitializeComponent();
+    }
+}
