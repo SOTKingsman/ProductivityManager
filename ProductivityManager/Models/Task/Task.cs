a9925d9e1d57d008bdd99fbcee73137d1d961611
nodnaL <lmiura@student.neumont.edu>
2026-08-12 16:04:41 -0600

    Task Update

--- ProductivityManager/Models/Task/Task.cs
+++ ProductivityManager/Models/Task/Task.cs
@@ -1,9 +1,11 @@
 using System.DirectoryServices.ActiveDirectory;
+using System.Text;
 
 namespace ProductivityManager.Views.Models;
 
 public class Task
 {
+    private StringBuilder sb;
     private String TaskName
     {
         get;
@@ -21,6 +23,17 @@ public class Task
         set
         {
             if (value != null || !value.Trim().Equals(""))
+            {
+                field = value;
+            }
+        }
+    }
+    private DateOnly Date
+    {
+        get;
+        set
+        {
+            if (value != null)
             {
                 field = value;
             }
@@ -49,17 +62,34 @@ public class Task
         }
     }
 
-    public Task(String taskName,String description,int startHour,int startMinute,int endHour,int endMinute)
+    public Task(String taskName,String description,int year,int month,int day,int startHour,int startMinute,int endHour,int endMinute)
     {
         TaskName = taskName;
         Description = description;
+        Date = SelectDate(year, month, day);
         StartTime = SelectTime(startHour, startMinute);
         EndTime = SelectTime(endHour, endMinute);
     }
 
+    public DateOnly SelectDate(int year, int month, int day)
+    {
+        DateOnly date = new DateOnly(year, month, day);
+        return date;
+    }
+    
     public TimeOnly SelectTime(int hour,int minute)
     {
         TimeOnly time = new TimeOnly(hour, minute);
         return  time;
+    }
+
+    public override string ToString()
+    {
+        return sb.Append("Task: ").Append(TaskName)
+            .AppendLine("Description: ").Append(Description)
+            .AppendLine("Date: ").Append(Date)
+            .AppendLine("Start Time: ").Append(StartTime)
+            .AppendLine("End Time: ").Append(EndTime)
+            .ToString();
     }
 }
