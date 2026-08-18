d8276571778f07a2a3af47c0ed8118c34e3fb6c9
MLazoM <aquteasever@gmail.com>
2026-08-18 15:23:22 -0600

    database template, file persistence next

---
+++ ProductivityManager/Models/TaskDatabase/Database.cs
@@ -0,0 +1,31 @@
+﻿namespace ProductivityManager.Views.Models.TaskDatabase;
+
+public class Database
+{
+    private List<Task> _tasks = new List<Task>();
+
+    public bool addTask (Task task)
+    {
+        if (task == null)
+        {
+            return false;
+        }
+        
+        _tasks .Add(task);
+        return true;
+    }
+
+    public bool deleteTask(Task task)
+    {
+        return _tasks.Remove(task);
+    }
+    
+    public List<Task> GetTasks()
+    {
+        return _tasks;
+    }
+    
+
+
+
+}
