79812ae8c07793417d4f798a6b16ac22c5a410b7
Ammon Wanner <sonofthekingsman51@gmail.com>
2026-08-25 01:57:36 -0600

    Added task editing and moved all of what we need into view models rather than code behind.

---
+++ ProductivityManager/ViewModels/TaskEditorViewModel.cs
@@ -0,0 +1,237 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using ProductivityManager.Commands;
+using ProductivityManager.Models.Task;
+
+namespace ProductivityManager.ViewModels;
+
+public class TaskEditorViewModel : INotifyPropertyChanged
+{
+    public event Action<TaskModel>? TaskCreated;
+    public event Action<TaskModel, TaskModel>? TaskEdited;
+    private TaskModel? _editingTask;
+    public string SubmitButtonText => _editingTask == null ? "Create" : "Confirm";
+    public string EditorTitle => _editingTask == null ? "Create Task" : "Edit Task";
+
+    public ICommand CreateTaskCommand { get; }
+    public ICommand CancelCommand { get; }
+
+    #region Task Properties
+    private string _taskName = "";
+    public string TaskName
+    {
+        get => _taskName;
+        set
+        {
+            _taskName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _category = "";
+    public string Category
+    {
+        get => _category;
+        set
+        {
+            _category = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _description = "";
+    public string Description
+    {
+        get => _description;
+        set
+        {
+            _description = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private DateTime? _startDate;
+    public DateTime? StartDate
+    {
+        get => _startDate;
+        set
+        {
+            _startDate = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private DateTime? _endDate;
+    public DateTime? EndDate
+    {
+        get => _endDate;
+        set
+        {
+            _endDate = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _startTime = "12:00 PM";
+    public string StartTime
+    {
+        get => _startTime;
+        set
+        {
+            _startTime = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _endTime = "1:00 PM";
+    public string EndTime
+    {
+        get => _endTime;
+        set
+        {
+            _endTime = value;
+            OnPropertyChanged();
+        }
+    }
+    #endregion
+
+    private bool _isOpen = false;
+    public bool IsOpen
+    {
+        get => _isOpen;
+        set
+        {
+            _isOpen = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _errorMessage = "";
+    public string ErrorMessage
+    {
+        get => _errorMessage;
+        set
+        {
+            _errorMessage = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public TaskEditorViewModel()
+    {
+        CreateTaskCommand = new RelayCommand(_ => CreateTask());
+        CancelCommand = new RelayCommand(_ => Cancel());
+
+        ClearInputs();
+    }
+
+    public void OpenCreate()
+    {
+        _editingTask = null;
+        OnPropertyChanged(nameof(EditorTitle));
+        OnPropertyChanged(nameof(SubmitButtonText));
+
+        ClearInputs();
+        IsOpen = true;
+    }
+
+    private void Cancel()
+    {
+        _editingTask = null;
+        IsOpen = false;
+        ClearInputs();
+    }
+
+    private void CreateTask()
+    {
+        ErrorMessage = "";
+
+        if (!DateTime.TryParse(StartTime, out DateTime parsedStartTime) ||
+            !DateTime.TryParse(EndTime, out DateTime parsedEndTime))
+        {
+            ErrorMessage = "Please enter valid start and end times.";
+            return;
+        }
+
+        if (!StartDate.HasValue || !EndDate.HasValue)
+        {
+            ErrorMessage = "Please select both a start date and an end date.";
+            return;
+        }
+
+        TimeSpan startTime = parsedStartTime.TimeOfDay;
+        TimeSpan endTime = parsedEndTime.TimeOfDay;
+
+        DateTime finalStart = StartDate.Value.Date + startTime;
+        DateTime finalEnd = EndDate.Value.Date + endTime;
+
+        if (finalEnd < finalStart)
+        {
+            ErrorMessage = "The end time cannot be before the start time.";
+            return;
+        }
+
+        TaskModel task = new TaskModel(
+            TaskName,
+            Category,
+            Description,
+            finalStart,
+            finalEnd
+        );
+
+        if (_editingTask == null)
+        {
+            TaskCreated?.Invoke(task);
+        }
+        else
+        {
+            TaskEdited?.Invoke(_editingTask, task);
+        }
+
+        _editingTask = null;
+        IsOpen = false;
+        ClearInputs();
+    }
+
+    private void ClearInputs()
+    {
+        TaskName = "";
+        Category = "";
+        Description = "";
+        StartDate = DateTime.Now;
+        EndDate = DateTime.Now.AddDays(1);
+        StartTime = "12:00 PM";
+        EndTime = "1:00 PM";
+        ErrorMessage = "";
+    }
+    
+    public void OpenEdit(TaskModel task)
+    {
+        _editingTask = task;
+        OnPropertyChanged(nameof(EditorTitle));
+        OnPropertyChanged(nameof(SubmitButtonText));
+
+        TaskName = task.TaskName;
+        Category = task.Category;
+        Description = task.Description;
+
+        StartDate = task.StartDateTime.Date;
+        StartTime = task.StartDateTime.ToString("h:mm tt");
+
+        EndDate = task.EndDateTime.Date;
+        EndTime = task.EndDateTime.ToString("h:mm tt");
+
+        IsOpen = true;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(
+            this,
+            new PropertyChangedEventArgs(propertyName)
+        );
+    }
+}
---
+++ ProductivityManager/ViewModels/TasksViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using ProductivityManager.Commands;
+using ProductivityManager.Models.Task;
+
+namespace ProductivityManager.ViewModels;
+
+public class TasksViewModel : INotifyPropertyChanged
+{
+    public TaskEditorViewModel Editor { get; } = new();
+    public ObservableCollection<TaskModel> Tasks { get; } = new();
+    
+    private TaskModel? _selectedTask;
+
+    public TaskModel? SelectedTask
+    {
+        get => _selectedTask;
+        set
+        {
+            _selectedTask = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public ICommand AddTaskCommand { get; }
+    
+    public ICommand EditTaskCommand { get; }
+
+    public TasksViewModel()
+    {
+        AddTaskCommand = new RelayCommand(_ => OpenCreateTask());
+        EditTaskCommand = new RelayCommand(task =>
+        {
+            if (task is TaskModel taskModel)
+            {
+                OpenEditTask(taskModel);
+            }
+        });
+
+        Editor.TaskCreated += OnTaskCreated;
+        Editor.TaskEdited += OnTaskEdited;
+    }
+    
+    private void OpenCreateTask()
+    {
+        Editor.OpenCreate();
+    }
+
+    private void OnTaskCreated(TaskModel task)
+    {
+        Tasks.Add(task);
+    }
+
+    public void OpenEditTask(TaskModel task)
+    {
+        Editor.OpenEdit(task);
+    }
+
+    private void OnTaskEdited(TaskModel oldTask, TaskModel newTask)
+    {
+        int index = Tasks.IndexOf(oldTask);
+
+        if (index >= 0)
+        {
+            Tasks[index] = newTask;
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(
+            this,
+            new PropertyChangedEventArgs(propertyName)
+        );
+    }
+}
